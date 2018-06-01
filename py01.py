1af06e1be2262e43afca43fe6a66caca0910c842
chen <[chenhao0427@gmail.com](mailto:chenhao0427@gmail.com)>
2018-06-01 08:56:20 +0800

    update

---
+++ py01.py
@@ -0,0 +1,60 @@
+# print('hello world')
+# name=input()
+# a=100
+# if a>100:
+# 	print(a)
+# else:
+# 	print(-a)
+
+# a=10/3
+# b=int(a)
+# print(a)
+# print(b)
+
+# def myabs(a):
+# 	try:
+# 		int(a)
+# 	except Exception as e:
+# 		raise
+# 	else:
+# 		pass
+# 	finally:
+# 		pass
+# 	if a>=0:
+# 		return a
+# 	else:
+# 		return -a
+# print(myabs(3))
+
+# def power(x,n):
+# 	s=1
+# 	while n>0:
+# 		n=n-1
+# 		s=s*x
+# 	return s
+# print(power(3,3))
+
+
+#递归调用
+# def fact(n):
+# 	if n==1:
+# 		return 1
+# 	return n*fact(n-1)
+# print(fact(4))
+
+# l=list(range(100))
+
+# print(l[:10])
+# print(l[-3:])
+
+# d={'a':1,'b':2,'c':3}
+# for k,v in enumerate(d):
+# 	print(k)
+# 	print(v)
+
+# list(range(1,10))
+# print([x*x for x in range(1,11)])
+#---————————————————————
+# import  os
+# print([d for d in os.listdir(',')])
+#---————————————————————
